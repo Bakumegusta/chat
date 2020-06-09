e33febdb2c7365d4b2a7df556fe035b619c59962
Bakumegusta <bakumegusta@gmail.com>
2020-06-10 01:10:33 +0530

    db

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
-
-app.listen(9000, ()=> console.log('listenng at 9000'));
+const Datastore = require('nedb');
+const port = process.env.PORT || 9000;
+app.listen(port, ()=> console.log(`listenng at ${port}`));
 app.use(express.static('public'));
 app.use(express.json());
 
+const database = new Datastore('chat.db');
+database.loadDatabase();
+
+app.get('/api', (request,response) =>{
+    database.find({}, (err, data) =>{
+    response.json(data);
+
+    })
+})
+
 app.post('/api', (request,response) =>{
-    console.log(request.body);
+    // console.log(request.body);
+    const data = request.body;
+    database.insert(data);
     response.json({
         status:'success',
     })
