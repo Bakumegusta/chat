6bf73729772ea11c6b470398aac6948b38fb4f8c
Bakumegusta <bakumegusta@gmail.com>
2020-06-15 00:30:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const port = process.env.PORT || 3000;
-// db connect
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const mongoose = require('mongoose')
+// const port = process.env.PORT || 3000;
+// // db connect
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('Connected to Database'))
 
-app.use(express.json())
+// app.use(express.json())
+// app.use(express.static('public'));
+
+// // routes
+// const chatdataRouter = require('./routes/chatdata')
+
+
+// // middleware
+// app.use('/chatdata', chatdataRouter)
+
+
+// // app listen
+// app.listen(port, () => console.log(`Server Started at port ${port}`))
+
+
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 9000;
+app.listen(port, ()=> console.log(`listenng at ${port}`));
 app.use(express.static('public'));
-
-// routes
-const chatdataRouter = require('./routes/chatdata')
-
-
-// middleware
-app.use('/chatdata', chatdataRouter)
-
-
-// app listen
-app.listen(port, () => console.log(`Server Started at port ${port}`))
+app.use(express.json());
