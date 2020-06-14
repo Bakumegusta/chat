efae1a7916e5afa90a0e90ecc9cc82efd8f38917
Bakumegusta <bakumegusta@gmail.com>
2020-06-15 01:16:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,16 @@
-// require('dotenv').config()
-// const express = require('express')
-// const app = express()
-// const mongoose = require('mongoose')
-// const port = process.env.PORT || 3000;
-// // db connect
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-// const db = mongoose.connection
-// db.on('error', (error) => console.error(error))
-// db.once('open', () => console.log('Connected to Database'))
-
-// app.use(express.json())
-// app.use(express.static('public'));
-
-// // routes
-// const chatdataRouter = require('./routes/chatdata')
-
-
-// // middleware
-// app.use('/chatdata', chatdataRouter)
-
-
-// // app listen
-// app.listen(port, () => console.log(`Server Started at port ${port}`))
-
-
-
-const express = require("express");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 const mongoose = require('mongoose')
-//  require('dotenv/config');
-const port = process.env.PORT || 9000;
-app.listen(port, ()=> console.log(`listenng at ${port}`));
-app.use(express.static('public'));
-app.use(express.json());
+const port = process.env.PORT || 3000;
+// db connect
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
+app.use(express.json())
+app.use(express.static('public'));
 
 // routes
 const chatdataRouter = require('./routes/chatdata')
@@ -42,8 +19,11 @@ const chatdataRouter = require('./routes/chatdata')
 // middleware
 app.use('/chatdata', chatdataRouter)
 
-// db connect
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+
+// app listen
+app.listen(port, () => console.log(`Server Started at port ${port}`))
+
+
+
+
+
